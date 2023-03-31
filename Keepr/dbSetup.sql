f6368db56f60a030b62c5a1b13ae974091fec7e6
Andrew <laxmeyers@gmail.com>
2023-03-31 11:11:46 -0600

    db set up

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -1,8 +1,23 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture',
+        coverImg VARCHAR(255)
+    ) default charset utf8 COMMENT '';
+
+Drop Table accounts;
+
+create Table
+    keeps(
+        id int not null PRIMARY KEY,
+        creatorId VARCHAR(255) not null,
+        name VARCHAR(100) not NULL,
+        description VARCHAR(255) not NULL,
+        img VARCHAR(255) not null,
+        views int not null DEFAULT 0,
+        Foreign Key (creatorId) REFERENCES accounts(id) on delete CASCADE
+    ) default charset utf8 COMMENT '';
