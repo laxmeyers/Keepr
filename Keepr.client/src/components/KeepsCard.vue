9957620862e13164e57b5054072a6165b14481f6
Andrew <laxmeyers@gmail.com>
2023-04-03 09:37:48 -0600

    keep card on homepage

---
+++ Keepr.client/src/components/KeepsCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="mb-3 position-relative">
+        <img :src="keep.img" class="img-fluid rounded selectable" alt="">
+        <div class="position-absolute keep-title">
+            <h5>{{ keep.name }}</h5>
+        </div>
+        <div class="position-absolute keep-creator">
+            <img :src="keep.creator.picture" class="rounded-circle profile-img selectable" :title="keep.creator.name" alt="">
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    props: { keep: { type: Object, required: true } },
+    setup() {
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.keep-title{
+    color: white;
+   bottom:8px;
+   left: 10px;
+}
+
+.keep-creator{
+    bottom: 8px;
+    right: 10px;
+    .profile-img{
+        height: 6vh;
+        width: 6vh;
+    }
+}
+</style>
--- Keepr.client/src/pages/HomePage.vue
+++ Keepr.client/src/pages/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
-{{ keeps }}
+<div class="container my-3">
+  <div class="bricks">
+    <div v-for="k in keeps">
+    <KeepsCard :keep="k"/>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -7,27 +13,32 @@ import Pop from '../utils/Pop';
 import {keepsService} from '../services/KeepsService'
 import { computed, onMounted } from 'vue';
 import { AppState } from '../AppState';
+import KeepsCard from '../components/KeepsCard.vue';
 
 export default {
-  setup() {
-    async function GetKeeps() {
-      try {
-        await keepsService.GetKeeps();
-      } catch (error) {
-        Pop.error(error, '[getting keeps]')       
-      }
-    } 
-
-    onMounted(() => {
-      GetKeeps()
-    })
-    return {
-      keeps: computed(() => AppState.keeps)
-    }
-  }
+    setup() {
+        async function GetKeeps() {
+            try {
+                await keepsService.GetKeeps();
+            }
+            catch (error) {
+                Pop.error(error, "[getting keeps]");
+            }
+        }
+        onMounted(() => {
+            GetKeeps();
+        });
+        return {
+            keeps: computed(() => AppState.keeps)
+        };
+    },
+    components: { KeepsCard }
 }
 </script>
 
 <style scoped lang="scss">
-
+.bricks {
+  columns: 300px;
+  column-gap:1em;
+}
 </style>
