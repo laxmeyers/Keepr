37cdb0d574c1e8681e11d68c8a33999c86ac0571
Andrew <laxmeyers@gmail.com>
2023-04-04 10:04:30 -0600

    drop down works better

--- Keepr.client/src/components/Login.vue
+++ Keepr.client/src/components/Login.vue
@@ -5,14 +5,14 @@
       Login
     </button>
     <div v-else>
-      <div class="dropdown dropstart my-2 my-lg-0">
+      <div class="dropdown-toggle text-dark pt-3 my-2 my-lg-0">
         <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown"
           aria-expanded="false">
           <div v-if="account.picture || user.picture">
             <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded-circle" />
           </div>
         </div>
-        <div class="dropdown-menu dropdown-menu-lg-left p-0" aria-labelledby="authDropdown">
+        <div class="dropdown-menu dropdown-menu-lg-end p-0" aria-labelledby="authDropdown">
           <div class="list-group">
             <router-link :to="{ name: 'Account' }">
               <div class="list-group-item dropdown-item list-group-item-action">
